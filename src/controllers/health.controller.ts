a607fb9b2cd2baab72e172934e49e5841bbb015a
paka <paka@emmental.co.kr>
2023-09-15 11:38:56 +0900

    feat: 이모지도 대답으로 사용하게 추가

--- src/controllers/health.controller.ts
+++ src/controllers/health.controller.ts
@@ -10,7 +10,11 @@ export class HealthController {
     this.dialogues = [
       '저, 저는··· 기타리스트로서 모두의 소중한 결속 밴드를 최고의 밴드로 만들고 싶어요.',
       '호... 혹시 모르니 만들지 않으실래요? 테루테루보즈..',
-      '존나 못 쳐서 죄송합니다앍!!!'
+      '존나 못 쳐서 죄송합니다앍!!!',
+      ':bocchi:',
+      ':bocchi-panic:',
+      ':bocchi-panic2:',
+      ':bocchi-panic3:'
     ];
   }
 
