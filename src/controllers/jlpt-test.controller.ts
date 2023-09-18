c66b76a7a652a86ee2bdf4be322c76c056267183
kayla-zent <kayla@zent.kr>
2023-09-18 09:03:43 +0900

    시험 디데이 커맨드 단일화 및 luxon 패키지 이용

--- src/controllers/jlpt-test.controller.ts
+++ src/controllers/jlpt-test.controller.ts
@@ -1,6 +1,7 @@
 import { Controller } from '@nestjs/common';
 import { Message } from 'nestjs-slack-bolt';
 import { SlackEventMiddlewareArgs } from '@slack/bolt';
+import {DateTime} from "luxon";
 
 @Controller()
 export class JlptTestController {
@@ -10,12 +11,13 @@ export class JlptTestController {
     this.testday = '20231203';
   }
 
-  @Message('!디데이' || '!시험')
+  @Message('!디데이')
   async dDday({ say }: SlackEventMiddlewareArgs) {
-    const today = new Date();
-    const targetDate = new Date(this.testday);
-    const difference = targetDate.getTime() - today.getTime();
-    const dday = Math.floor(difference / (1000 * 60 * 60 * 24));
+    const dday =
+      DateTime.fromISO(this.testday)
+        .diff(DateTime.local())
+        .get('milliseconds') /
+      (1000 * 60 * 60 * 24);
     await say(
       `2023년도 제 2회 JLPT 시험일 12월 3일(일요일) 까지 ${dday}일 남았습니다.`
     );
