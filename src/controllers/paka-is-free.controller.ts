fa7ca4c14568770737e06395a29f13a2b52600bb
kayla-zent <kayla@zent.kr>
2023-12-02 15:49:50 +0900

    디데이 명령어 invisible처리 및 파카 명령어 추가로 파카 전역일 확인 기능으로 변경

--- src/controllers/paka-is-free.controller.ts
+++ src/controllers/paka-is-free.controller.ts
@@ -4,23 +4,23 @@ import { SlackEventMiddlewareArgs } from '@slack/bolt';
 import {DateTime} from "luxon";
 
 @Controller()
-export class JlptTestController {
-  private readonly testday: string;
+export class PakaIsFreeController {
+  private readonly theDayOfFreedom: string;
 
   constructor() {
-    this.testday = '20231203';
+    this.theDayOfFreedom = '20251104';
   }
 
-  @Message('!디데이')
+  @Message('!파카')
   async dDday({ say }: SlackEventMiddlewareArgs) {
     const dday = Math.ceil(
-      DateTime.fromISO(this.testday)
+      DateTime.fromISO(this.theDayOfFreedom)
         .diff(DateTime.local())
         .get('milliseconds') /
         (1000 * 60 * 60 * 24)
     );
     await say(
-      `2023년도 제 2회 JLPT 시험일 12월 3일(일요일) 까지 ${dday}일 남았습니다.`
+      `파카의 전역일인 2025년 11월 4일까지 ${dday}일 남았습니다.:경례를_하는_얼굴:`
     );
   }
 }
